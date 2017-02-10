005e0f848f41468723db29bac3e4a32480eb457b
Vladlen <vladlen.fedorenko@gmail.com>
2017-02-10 20:21:38 +0200

    Update te-parser.py

--- te-parser.py
+++ te-parser.py
@@ -1,10 +1,30 @@
 import docx2txt
 
+script_format = """from TextEncounter import TextEncounter
+
+
+class TextEncounter{ID}(TextEncounter):
+    def __init__(self):
+        super(TextEncounter{ID}, self).__init__()
+        self.id = "{ID}"
+        self.name = "{Name}"
+{LineConditions}
+        pass
+
+    def _onCheckConditions(self, context):{ContextConditions}
+        return True
+        pass
+
+    def _onGenerate(self, context, dialog):{Dialog}
+        pass
+    pass
+"""
+
+text_id_format = "<Text Key=\"{key}\" Value=\"{value}\"/>"
+
 # utils for parsing
 def delete_comments(lines):
-    """
-    Clear text from comments like //comment
-    """
+    """Clear text from comments like //comment"""
     result = []
     for line in lines:
         # delete comments from line
@@ -19,9 +39,7 @@ def delete_comments(lines):
     return result
 
 def get_tag(word):
-    """
-    Check word for tag
-    """
+    """Check word for tag"""
     if word.count(":") != 1:
         return None
     if word.endswith(":") is False:
@@ -29,8 +47,7 @@ def get_tag(word):
     return word[:-1]
 
 def parse_nodes(text):
-    """
-    Parsing nodes from text
+    """Parsing nodes from text
     node = tag, value
     """
     lines = text.split("\n\n")
@@ -66,78 +83,130 @@ def parse_nodes(text):
     return nodes
     pass
 
-# main function
+def write_script(text, file_name):
+    """Write python script"""
+    with open(file_name, "w") as f:
+        f.write(text)
+        pass
+    pass
+
+def add_debug_text(script_text, nodes, texts):
+    """Add debug text as multi line comment to script text"""
+    debug_format = """{script}
+\"\"\" debug info
+------------- Nodes -------------
+{nodes}
+------------- Texts -------------
+{texts}
+\"\"\"
+"""
+    nodes_str = "\n".join(map(lambda (t, v): "{} = \"{}\"".format(t, v), nodes))
+    texts_str = "\n".join(map(lambda text: text_id_format.format(**text), texts))
+    full_text = debug_format.format(script=script_text, nodes=nodes_str, texts=texts_str)
+    return full_text
+    pass
+
+# ----------------------- PARSE -----------------------
 def parse(text, file_name):
     nodes = parse_nodes(text)
 
-    result = """
-from TextEncounter import TextEncounter
-
-
-class TextEncounter{ID}(TextEncounter):
-    def __init__(self):
-        super(TextEncounter{ID}, self).__init__()
-        self.id = "{ID}"
-        self.name = "{Name}"
-        self.priority = {Priority}
-        self.occurrence = {Occurrence}
-        self.frequency = {Frequency}
-
-        self.planet = "{Planet}"
-        self.levels = {Levels}
-        pass
-    def _onCheckConditions(self, context):
-        return True
-        pass
-    def _onGenerate(self, context, dialog):
-        pass
-    pass
-"""
     params = dict(
-        ID=None,
-        Name=None,
-        Priority=None,
-        Occurrence=None,
-        Frequency=None,
-        Planet=None,
-        Levels=None,
+        ID="000",
+        Name="NoName",
+        LineConditions="",
+        ContextConditions="",
+        Dialog="",
     )
 
-    debug_text = ""
+    indentation = "        "
+    texts = []
 
-    # process nodes
-    text_ids = ""
-    text_id_format = "<Text Key=\"{id}\" Value=\"{value}\"/>\n"
+    option_index = 0
+    outcome_index = 0
+    entity = None
+
     for tag, value in nodes:
-        if tag in params:
-            params[tag] = value
-        #dummy
-        if tag == "Dialog" and value:
-            text_ids += text_id_format.format(id="ID_TE_Dialog", value=value)
-
-        if tag == "Option" and value:
-            text_ids += text_id_format.format(id="ID_TE_Option", value=value)
-
-        if tag == "Outcome" and value:
-            text_ids += text_id_format.format(id="ID_TE_Outcome", value=value)
-
-        debug_text += "{} = \"{}\"\n".format(tag, value)
+        if tag == "Conditions":
+            entity = "conditions"
+        elif tag == "ID":
+            params["ID"] = value
+        elif tag == "Name":
+            params["Name"] = value
+        elif tag == "Planet":
+            params["LineConditions"] += "\n{}self.planet = \"{}\"".format(indentation, value)
+        elif tag == "Levels":
+            lvl_from, lvl_to = map(int, value.split())
+            params["LineConditions"] += "\n{}self.levels = range({}, {})".format(indentation, lvl_from, lvl_to + 1)
+        elif tag == "Priority":
+            params["LineConditions"] += "\n{}self.priority = {}".format(indentation, int(value))
+        elif tag == "Occurrence":
+            params["LineConditions"] += "\n{}self.occurrence = \"{}\"".format(indentation, value)
+            pass
+        elif tag == "Frequency":
+            params["LineConditions"] += "\n{}self.frequency = {}".format(indentation, int(value))
+            pass
+        elif tag == "Mech1":
+            if entity is "conditions":
+                params["ContextConditions"] += "\n{}if context.mech1.dummy() is False:  # {}".format(indentation, value)
+                params["ContextConditions"] += "\n{}    return False\n".format(indentation)
+            pass
+        elif tag == "Mech2":
+            pass
+        elif tag == "Cargo":
+            params["ContextConditions"] += "\n{}if context.cargo.dummy() is False:  # {}".format(indentation, value)
+            params["ContextConditions"] += "\n{}    return False\n".format(indentation)
+        elif tag == "Dialog":
+            if value:
+                text = dict(
+                    key="ID_TE_{}_Dialog".format(params["ID"]),
+                    value=value
+                )
+                texts.append(text)
+                params["Dialog"] += "\n{}dialog.text = \"{}\"".format(indentation, text["key"])
+        elif tag == "Option":
+            entity = "option"
+            params["Dialog"] += "\n\n{}option = dialog.option()".format(indentation)
+            if value:
+                option_index += 1
+                text = dict(
+                    key="ID_TE_{}_Option_{}".format(params["ID"], option_index),
+                    value=value
+                )
+                texts.append(text)
+                params["Dialog"] += "\n{}{}.text = \"{}\"".format(indentation, entity, text["key"])
+        elif tag == "Outcome":
+            entity = "outcome"
+            params["Dialog"] += "\n\n{}outcome = dialog.outcome()".format(indentation)
+            if value:
+                outcome_index += 1
+                text = dict(
+                    key="ID_TE_{}_Outcome_{}".format(params["ID"], option_index),
+                    value=value
+                )
+                texts.append(text)
+                params["Dialog"] += "\n{}{}.text = \"{}\"".format(indentation, entity, text["key"])
+        elif tag == "Gips":
+            params["Dialog"] += "\n{}{}.gips = \"{}\"".format(indentation, entity, value)
+        elif tag == "Chance":
+            params["Dialog"] += "\n{}{}.chance = \"{}\"".format(indentation, entity, int(value))
+        elif tag == "Chance":
+            params["Dialog"] += "\n{}{}.chance = \"{}\"".format(indentation, entity, int(value))
+        elif tag == "Combat":
+            params["Dialog"] += "\n\n{}combat = {}.option()".format(indentation, entity)
+            entity = "combat"
+        elif tag == "Enemy1":
+            params["Dialog"] += "\n{}{}.enemy1 = \"{}\"".format(indentation, entity, value)
+        elif tag == "LoadTE":
+            params["Dialog"] += "\n{}{}.loadTE = \"{}\"".format(indentation, entity, value.strip(" "))
         pass
 
-    result = result.format(**params)
-
-    # dummy write files
-    with open(file_name, "w") as file:
-        file.write(result)
-        file.write("\n\"\"\" debug info\n")
-        file.write("--------- ID - Value ---------\n")
-        file.write(debug_text)
-        file.write("----------- Texts -----------\n")
-        file.write(text_ids)
-        file.write("\"\"\"")
-        pass
-
+    script_text = script_format.format(**params)
+    # add debug text
+    script_text = add_debug_text(script_text, nodes, texts)
+    # dummy write
+    write_script(script_text, file_name)
     pass
+# -----------------------------------------------------
 
 # debug tests
 if __name__ == '__main__':
