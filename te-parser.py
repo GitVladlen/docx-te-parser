a5f6d18be9d845f15e4783b8b99e2b4addefef58
Vladlen <vladlen.fedorenko@gmail.com>
2017-02-18 21:48:09 +0200

    Refactor

--- te-parser.py
+++ te-parser.py
@@ -14,7 +14,7 @@ def process(docx_file_path):
     # text parsing text and creating script file
     return parse_text(text)
     pass
-    
+
 # -------------------------------- UTILS --------------------------------
 def parse_text(text):
     # get nodes from text
@@ -64,22 +64,23 @@ def parse_nodes(text):
         "Scrap",
         "LoadTE",
     ]
-
+    # gain text without comments and joined by space
     text = delete_comments_from_text(text)
 
-    line_text_decoded = text.encode()
+    text_bytes = text.encode()
 
-    bones = "|".join(tag_list)
-
-    tags_regex = r'({}):'.format(bones)
-    tags = re.findall(tags_regex, line_text_decoded.decode())
-
+    all_tags_joined = "|".join(tag_list)
+    # find tags
+    tags_regex = r'({}):'.format(all_tags_joined)
+    tags = re.findall(tags_regex, text_bytes.decode())
+    # match values
     values_regex = r'{}'.format(":(.*)".join(tags) + ":(.*)")
-    value_matches = re.match(values_regex, line_text_decoded.decode())
+    value_matches = re.match(values_regex, text_bytes.decode())
+    # gain values from values matches
     values = []
     if value_matches:
         values = value_matches.groups()
-
+    # pack nodes
     nodes = list(zip(tags, values))
     return nodes
     pass
@@ -104,6 +105,27 @@ def delete_comments_from_text(text):
     result = delete_comments(lines)
 
     return ' '.join(result)
+    pass
+
+def split_nodes_by_encounters(nodes):
+    # todo: refactor to regex
+    result = []
+    cur_nodes = []
+    for node in nodes:
+        tag, _ = node
+        
+        if tag == "ID":
+            if cur_nodes:
+                result.append(cur_nodes)
+            cur_nodes = []
+
+        cur_nodes.append(node)
+
+    if cur_nodes:
+        result.append(cur_nodes)
+
+    # print "".join(map(lambda (index, nodes): "{}. {}\n\n".format(index, nodes), enumerate(result)))
+    return result
     pass
 
 def format_texts(all_texts):
@@ -133,27 +155,6 @@ def add_debug_text(script_text, nodes, texts):
     return full_text
     pass
 
-def split_nodes_by_encounters(nodes):
-    # todo: refactor to regex
-    result = []
-    cur_nodes = []
-    for node in nodes:
-        tag, _ = node
-        
-        if tag == "ID":
-            if cur_nodes:
-                result.append(cur_nodes)
-            cur_nodes = []
-
-        cur_nodes.append(node)
-
-    if cur_nodes:
-        result.append(cur_nodes)
-
-    # print "".join(map(lambda (index, nodes): "{}. {}\n\n".format(index, nodes), enumerate(result)))
-    return result
-    pass
-
 def get_id_from_nodes(nodes):
     for tag, value in nodes:
         if tag == "ID":
@@ -162,6 +163,8 @@ def get_id_from_nodes(nodes):
 
 # -------------------------------- NODES PARSING --------------------------------
 def parse_data(nodes):
+    # todo: modify parsing alghoriphm
+
     te_id = get_id_from_nodes(nodes)
 
     params = dict(
@@ -489,49 +492,46 @@ text_id_format = "<Text Key=\"{key}\" Value=\"{value}\"/>"
 
 # -------------------- STUFF FOR EXECUTE FROM PACKAGE (AS MAIN) --------------------
 def process_main(docx_file_name, destination_dir):
+    # define filenames
     texts_filename = os.path.join(destination_dir, "TextEncounterTexts.xml")
     register_filename = os.path.join(destination_dir, "te-register.txt")
-
+    # define string formats for writing register file as table
     register_title_format = "|{ID:^8}|{Module:^16}|{TypeName:^20}|\n"
     register_row_format = "|{ID:>8}|{Module:>16}|{TypeName:>20}|\n"
     register_line_string = "=" * (8 + 16 + 20 + 4) + "\n"
-
+    # do job, gain result scrips and texts
     scripts, texts = process(docx_file_name)
-
+    # write texts
     with open(texts_filename, "w") as f:
             f.write(texts)
-    
+    # write register and scrips
     with open(register_filename, "w") as register:
+        # write register title
         register.write(register_line_string)
         register.write(register_title_format.format(ID="ID", Module="Module", TypeName="TypeName"))
         register.write(register_line_string)
-
+        # write each script to corresponding file
         for r, (te_id, text) in enumerate(scripts, start=1):
-
             type_name = "TextEncounter{}".format(te_id)
             generateFilename = os.path.join(destination_dir, type_name + ".py")
-
+            # write script
             with open(generateFilename, "w") as f:
                 f.write(text)
-
+            # write record about script in register
             register.write(register_row_format.format(ID=te_id, Module="TextEncounter", TypeName=type_name))
             pass
         pass
-
+        # write line in end of register
         register.write(register_line_string)
     pass
 
 def process_args():
-    parser = argparse.ArgumentParser(
-        description="Export Text Encounter from docx file to Mengine"
-        )
+    parser = argparse.ArgumentParser(description="Export Text Encounter from docx file to Mengine")
     parser.add_argument("--docx", help="path of the docx file")
     parser.add_argument("--dest", help="path of destination directory")
 
     args = parser.parse_args()
-        
     return args
-    pass
 
 # ---------------------------------------------------------------------------------------------
 if __name__ == '__main__':
@@ -539,28 +539,25 @@ if __name__ == '__main__':
     cur_dir_path = os.path.dirname(__file__)
     destination_dir = os.path.join(cur_dir_path, "debug/")
     docx_file_name = os.path.join(destination_dir, "te-format.docx")
-
     # check console args
     args = process_args()
     if args.docx and args.dest:
         docx_file_name = args.docx
         destination_dir = args.dest
         pass
-
+    # check docx file existance
     if not os.path.exists(docx_file_name):
         print("File {} dose not exist.".format(docx_file_name))
         sys.exit(1)
         pass
-
+    # check destination dir existance
     if not os.path.exists(destination_dir):
         try:
             os.makedirs(destination_dir)
         except OSError:
             print("Unable to create destination dir {}".format(destination_dir))
             sys.exit(1)
-        pass
-
-    # execute
+    # process
     process_main(docx_file_name, destination_dir)
     pass
 # ---------------------------------------------------------------------------------------------
