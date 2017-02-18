1775300249e15581348feaefff09524fd839ed76
Vladlen <vladlen.fedorenko@gmail.com>
2017-02-18 21:18:38 +0200

    Refactor

--- te-parser.py
+++ te-parser.py
@@ -1,26 +1,23 @@
 import sys
 import os
-import docx2txt
 import re
 import argparse
 
+import docx2txt
 # ---------------------------- FORMAT STRINGS ----------------------------
 script_format = """from Game.TextEncounters.TextEncounter import TextEncounter
 
 
 class TextEncounter{ID}(TextEncounter):
     def __init__(self):
-        super(TextEncounter{ID}, self).__init__()
-        self.id = "{ID}"
-        self.name = "{Name}"
-{LineConditions}
+        super(TextEncounter{ID}, self).__init__(){InitParams}
         pass
 
-    def _onCheckConditions(self, context):{ContextConditions}
+    def _onCheckConditions(self, context):{CheckConditions}
         return True
         pass
 
-    def _onGenerate(self, context, dialog):{Dialog}
+    def _onGenerate(self, context, dialog):{GenerateDialog}
         pass
     pass
 """
@@ -56,52 +53,7 @@ def delete_comments_from_text(text):
     return ' '.join(result)
     pass
 
-def get_tag(word):
-    """Check word for tag"""
-    if word.count(b":") != 1:
-        return None
-    if word.endswith(b":") is False:
-        return None
-    return word[:-1]
-
 def parse_nodes(text):
-    """Parsing nodes from text
-    node = tag, value
-    """
-    lines = text.split(b'\n\n')
-    lines = delete_comments(lines)
-
-    word_delimiter = b" "
-    args = []
-    last_tag = None
-
-    nodes = []
-
-    for line in lines:
-        words = line.split(word_delimiter)
-        for word in words:
-            tag = get_tag(word)
-            if tag is None:
-                args.append(word.decode())
-                continue
-
-            if last_tag is not None:
-                value = " ".join(args)
-                value = value.replace(" [br] ", "&#10;")
-                node = last_tag.decode(), value
-                nodes.append(node)
-
-                args = []
-                last_tag = tag
-            else:
-                last_tag = tag
-            pass
-        pass
-
-    return nodes
-    pass
-
-def parse_nodes2(text):
     """Parsing nodes from text
     node = tag, value
     """
@@ -150,34 +102,6 @@ def parse_nodes2(text):
     return nodes
     pass
 
-def write_script(script_text, dir_path, te_id):
-    """Write python script"""
-    # do full file path
-    file_name = "TextEncounter{ID}.py".format(ID=te_id)
-    path = os.path.join(dir_path, file_name)
-    print(path)
-    # write
-    with open(path, "w") as f:
-        f.write(script_text)
-        pass
-    pass
-
-def write_texts(all_texts, dir_path):
-    file_name = "Texts.xml"
-    path = os.path.join(dir_path, file_name)
-    print(path)
-    all_texts_str_list = []
-    for texts in all_texts:
-        texts_str = "\n\t".join(map(lambda text: text_id_format.format(**text), texts))
-        all_texts_str_list.append(texts_str)
-    all_texts_str = "\n\n\t".join(all_texts_str_list)
-
-    texts_to_write = texts_format.format(Texts=all_texts_str)
-    with open(path, "w") as f:
-        f.write(texts_to_write)
-        pass
-    pass
-
 def format_texts(all_texts):
     all_texts_str_list = []
     for texts in all_texts:
@@ -206,6 +130,7 @@ def add_debug_text(script_text, nodes, texts):
     pass
 
 def split_nodes_by_encounters(nodes):
+    # todo: refactor to regex
     result = []
     cur_nodes = []
     for node in nodes:
@@ -225,126 +150,13 @@ def split_nodes_by_encounters(nodes):
     return result
     pass
 
-def parse_data(nodes):
-    params = dict(
-        ID="000",
-        Name="NoName",
-        LineConditions="",
-        ContextConditions="",
-        Dialog="",
-    )
-
-    indentation = "        "
-    texts = []
-
-    option_index = 0
-    outcome_index = 0
-    entity = None
-
-    for tag, value in nodes:
-        if tag == "Conditions":
-            entity = "conditions"
-        elif tag == "ID":
-            params["ID"] = value
-        elif tag == "Name":
-            params["Name"] = value
-        elif tag == "Planet":
-            params["LineConditions"] += "\n{}self.planet = \"{}\"".format(indentation, value)
-        elif tag == "Levels":
-            lvl_from, lvl_to = map(int, value.split())
-            params["LineConditions"] += "\n{}self.levels = range({}, {})".format(indentation, lvl_from, lvl_to + 1)
-        elif tag == "Priority":
-            params["LineConditions"] += "\n{}self.priority = {}".format(indentation, int(value))
-        elif tag == "Occurrence":
-            params["LineConditions"] += "\n{}self.occurrence = \"{}\"".format(indentation, value)
-            pass
-        elif tag == "Frequency":
-            params["LineConditions"] += "\n{}self.frequency = {}".format(indentation, int(value))
-            pass
-        elif tag == "Mech1":
-            if entity is "conditions":
-                params["ContextConditions"] += "\n{}if context.mech1.dummy() is False:  # {}".format(indentation, value)
-                params["ContextConditions"] += "\n{}    return False\n".format(indentation)
-            pass
-        elif tag == "Mech2":
-            pass
-        elif tag == "Cargo":
-            params["ContextConditions"] += "\n{}if context.cargo.dummy() is False:  # {}".format(indentation, value)
-            params["ContextConditions"] += "\n{}    return False\n".format(indentation)
-        elif tag == "Dialog":
-            if value:
-                text = dict(
-                    key="ID_TE_{}_Dialog".format(params["ID"]),
-                    value=value
-                )
-                texts.append(text)
-                params["Dialog"] += "\n{}dialog.text = \"{}\"".format(indentation, text["key"])
-        elif tag == "Option":
-            entity = "option"
-            params["Dialog"] += "\n\n{}option = dialog.option()".format(indentation)
-            if value:
-                option_index += 1
-                text = dict(
-                    key="ID_TE_{}_Option_{}".format(params["ID"], option_index),
-                    value=value
-                )
-                texts.append(text)
-                params["Dialog"] += "\n{}{}.text = \"{}\"".format(indentation, entity, text["key"])
-        elif tag == "Outcome":
-            entity = "outcome"
-            params["Dialog"] += "\n\n{}outcome = dialog.outcome()".format(indentation)
-            if value:
-                outcome_index += 1
-                text = dict(
-                    key="ID_TE_{}_Outcome_{}".format(params["ID"], option_index),
-                    value=value
-                )
-                texts.append(text)
-                params["Dialog"] += "\n{}{}.text = \"{}\"".format(indentation, entity, text["key"])
-        elif tag == "Gips":
-            params["Dialog"] += "\n{}{}.gips = \"{}\"".format(indentation, entity, value)
-        elif tag == "Chance":
-            params["Dialog"] += "\n{}{}.chance = \"{}\"".format(indentation, entity, int(value))
-        elif tag == "Chance":
-            params["Dialog"] += "\n{}{}.chance = \"{}\"".format(indentation, entity, int(value))
-        elif tag == "Combat":
-            params["Dialog"] += "\n\n{}combat = {}.option()".format(indentation, entity)
-            entity = "combat"
-        elif tag == "Enemy1":
-            params["Dialog"] += "\n{}{}.enemy1 = \"{}\"".format(indentation, entity, value)
-        elif tag == "LoadTE":
-            params["Dialog"] += "\n{}{}.loadTE = \"{}\"".format(indentation, entity, value.strip(" "))
-        pass
-
-    script_text = script_format.format(**params)
-    # add debug text
-    script_text = add_debug_text(script_text, nodes, texts)
-    return params["ID"], script_text, texts
-    pass
-
 def get_id_from_nodes(nodes):
     for tag, value in nodes:
         if tag == "ID":
             return value
     return None
 
-def parse_data_experiment(nodes):
-    script_format = """from Game.TextEncounters.TextEncounter import TextEncounter
-
-
-class TextEncounter{ID}(TextEncounter):
-    def __init__(self):
-        super(TextEncounter{ID}, self).__init__(){InitParams}
-        pass
-
-    def _onCheckConditions(self, context):{CheckConditions}
-        return True
-        pass
-
-    def _onGenerate(self, context, dialog):{GenerateDialog}
-        pass
-    pass
-"""
+def parse_data(nodes):
     te_id = get_id_from_nodes(nodes)
 
     params = dict(
@@ -643,50 +455,16 @@ class TextEncounter{ID}(TextEncounter):
 
     return params["ID"], script_text, params["Texts"]
     pass
-
-def process_text(text, dir_path):
-    # get nodes from text
-    all_nodes = parse_nodes(text)
-    nodes_by_encounters = split_nodes_by_encounters(all_nodes)
-    # transform nodes to files
-    all_texts = []
-    for nodes in nodes_by_encounters:
-        # te_id, script_text, texts = parse_data(nodes)
-        te_id, script_text, texts = parse_data_experiment(nodes)
-        # accumulate all texts
-        all_texts.append(texts)
-        # write python script
-        write_script(script_text, dir_path, te_id)
-        pass
-    # write xml file with text resources
-    write_texts(all_texts, dir_path)
-    pass
-
-# ----------------------- USE THIS -----------------------
-def process_docx(docx_file_path, destination_dir):
-    # gain text from docx
-    text = docx2txt.process(docx_file_path)
-    text = text.encode('utf-8')
-
-    # write text to txt file, for debug
-    result_txt_file_name = os.path.join(destination_dir, "docx-to-text-result.txt")
-    with open(result_txt_file_name, "w") as file:
-        file.write(text)
-
-    # text parsing text and creating script file
-    process_text(text, destination_dir)
-    pass
 # ----------------- INTEGRATE TO MENGINE ----------------
 def parse_text(text):
     # get nodes from text
-    all_nodes = parse_nodes2(text)
+    all_nodes = parse_nodes(text)
     nodes_by_encounters = split_nodes_by_encounters(all_nodes)
     # transform nodes to files
     scripts = []
     all_texts = []
     for nodes in nodes_by_encounters:
-        # te_id, script_text, texts = parse_data(nodes)
-        te_id, script_text, texts = parse_data_experiment(nodes)
+        te_id, script_text, texts = parse_data(nodes)
         # accumulate all texts
         all_texts.append(texts)
         # accumulate all scripts
@@ -749,7 +527,7 @@ def process_args():
         
     return args
     pass
-
+# ---------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     # setup default file names
     cur_dir_path = os.path.dirname(__file__)
@@ -779,3 +557,4 @@ if __name__ == '__main__':
     # execute
     process_main(docx_file_name, destination_dir)
     pass
+# ---------------------------------------------------------------------------------------------
